7873db4c5a977bd4af45f32af998dc29a9f7653f
João Teodoro <joaotdn@gmail.com>
2020-01-12 09:45:31 -0300

    Parsing request bodies

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   //console.log(req.url, req.method, req.headers);
   //process.exit();
+  res.setHeader('Content-Type', 'text/html');
 
   if(req.url === '/') {
     res.write('<hmtl>');
@@ -10,13 +12,31 @@ const server = http.createServer((req, res) => {
     res.write('<title>TESTE</title>');
     res.write('</head>');
     res.write('<body>');
-    res.write('<form action="/message"><input type="text"><button type="submit">Send</button></form>');
+    res.write('<form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form>');
     res.write('</body>');
     res.write('</hmtl>');
     return res.end();
   }
 
-  res.setHeader('Content-Type', 'text/html');
+  if(req.url === '/message' && req.method === 'POST') {
+    const body = [];
+    
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    });
+
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
   res.write('<hmtl>');
   res.write('<head>');
   res.write('<title>TESTE</title>');

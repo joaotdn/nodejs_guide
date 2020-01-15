0263aff487d05f6545bf2a67c99693fe9b135d2d
João Teodoro <joaotdn@gmail.com>
2020-01-15 17:10:37 -0300

    testing debug

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
-
   res.setHeader("Content-Type", "text/html");
 
   if (url === "/") {

448ee3efb65cec14c14f4208e5b53b7fbdce1427
João Teodoro <joaotdn@gmail.com>
2020-01-16 08:49:09 -0300

    adding routes middlewares

--- app.js
+++ app.js
@@ -2,20 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">ADD</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.statusCode = 302;
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Express.js</h1>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use((req, res) => res.status(404).send('<h1>Page not found!</h1>'));
 
 app.listen(3000);

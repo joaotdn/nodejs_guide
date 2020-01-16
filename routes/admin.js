c6d992745e36bc6a75b5f27bcc4bb8a8c3ccf6c2
João Teodoro <joaotdn@gmail.com>
2020-01-16 09:36:08 -0300

    serving html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const path = require('path');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">ADD</button></form>'
-  );
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post("/add-product", (req, res, next) => {
